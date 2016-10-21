acb55ae12b7172470e3a0ef43ba3903360794e55
SUEHIRO <supersuehiro@users.sourceforge.jp>
2016-10-22 00:08:02 +0900

    glfw

--- src/main.go
+++ src/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "github.com/Shopify/go-lua"
+import (
+	"github.com/Shopify/go-lua"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"runtime"
+)
 
+func init() {
+	runtime.LockOSThread()
+}
 func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+	window, err := glfw.CreateWindow(640, 480, "Ikemen GO", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
 	l := lua.NewState()
 	lua.OpenLibraries(l)
 	if err := lua.DoFile(l, "script/main.lua"); err != nil {
 		panic(err)
+	}
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
 	}
 }
