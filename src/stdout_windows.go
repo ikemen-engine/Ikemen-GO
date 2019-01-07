8812e010f19cd943af4fddc728286258dcd761ac
suehiro <supersuehiro@users.sourceforge.jp>
2019-01-07 19:49:08 +0900

    GOOS=windows でビルドできなかったのを修正

--- src/stdout_windows.go
+++ src/stdout_windows.go
@@ -2,16 +2,19 @@
 package main
 
 import (
-	"syscal"
+	"os"
+	"syscall"
 )
 
-modkernel32 := syscall.NewLazyDLL("kernel32.dll")
-procAllocConsole := modkernel32.NewProc("AllocConsole")
-sscall.Syscall(procAllocConsole.Addr(), 0, 0, 0, 0)
-hout, err1 := syscall.GetStdHandle(syscall.STD_OUTPUT_HANDLE)
-hin, err2 := syscall.GetStdHandle(syscall.STD_INPUT_HANDLE)
-if err1 != nil || err2 != nil { // nowhere to print the message
-	s.Exit(2)
+func init() {
+	modkernel32 := syscall.NewLazyDLL("kernel32.dll")
+	procAllocConsole := modkernel32.NewProc("AllocConsole")
+	syscall.Syscall(procAllocConsole.Addr(), 0, 0, 0, 0)
+	hout, err1 := syscall.GetStdHandle(syscall.STD_OUTPUT_HANDLE)
+	hin, err2 := syscall.GetStdHandle(syscall.STD_INPUT_HANDLE)
+	if err1 != nil || err2 != nil { // nowhere to print the message
+		syscall.Exit(2)
+	}
+	os.Stdout = os.NewFile(uintptr(hout), "/dev/stdout")
+	os.Stdin = os.NewFile(uintptr(hin), "/dev/stdin")
 }
-os.Stdout = os.NewFile(uintptr(hout), "/dev/stdout")
-os.Stdin = os.NewFile(uintptr(hin), "/dev/stdin")
