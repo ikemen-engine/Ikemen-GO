abc98e8d86a0f4675285a99efd7b7397d8e3cedf
Gacel <GacelF@outlook.com>
2019-11-18 17:20:03 -0500

    Update get.sh

--- build/get.sh
+++ build/get.sh
@@ -37,7 +37,7 @@ go get -u github.com/yuin/gopher-lua
 go get -u github.com/go-gl/glfw/v3.2/glfw
 go get -u github.com/go-gl/gl/v2.1/gl
 go get -u github.com/timshannon/go-openal/openal
-go get -u github.com/K4thos/glfont
+go get -u github.com/Windblade-GR01/glfont
 go get -u github.com/flopp/go-findfont
 go get -u github.com/faiface/beep
 go get -u github.com/hajimehoshi/oto
