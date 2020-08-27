38c15573cd62e46b70b12c6bd2a09df497eec1cc
unknown <Windblade-Windblade-GR01@users.noreply.github.com>
2020-08-27 18:36:36 -0500

    Configurable GLSL version for font shaders

--- src/font.go
+++ src/font.go
@@ -342,6 +342,7 @@ func loadFntTtf(f *Fnt, fontfile string, filename string, height int32) {
 	} else {
 		f.Size[1] = uint16(height)
 	}
+	glfont.FontShaderVer = "#version " + sys.fontShaderVer
 	ttf, err := glfont.LoadFont(fileDir, height, int(sys.gameWidth), int(sys.gameHeight))
 	if err != nil {
 		panic(err)
