ad69e66b1fadcd87ac26a72159e53a11df0591ba
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2020-10-31 01:16:47 -0500

    Update appveyor_docker_build.sh

--- build/appveyor_docker_build.sh
+++ build/appveyor_docker_build.sh
@@ -1,29 +1,3 @@
-#!/bin/bash
-
-###############################################################################
-# Simple script that uses a docker image to build the binaries of Ikemen_GO for 
-# Windows/Linux/OSX plataforms
-# the only dependencies are the source code of Ikemen_GO_plus and docker.
-#
-# @author Daniel Porto 
-# https://github.com/danielporto
-# and edited by Gacel to allow to use on AppVeyor
-###############################################################################
-
-# Parameters explained:
-#  run  : download and execute the docker container with the building tools
-#  --rm : discard the container after using it. It saves disk space
-#  -e   : set environment variables used by the scripts called inside the container 
-#         these variables select the cross-compiling parameters invoked. 
-#         Look inside the get.sh and build.sh for details. 
-#  -v   : maps a volume (folder) inside the  container (makes the current source code accessible inside the container)
-#       $(pwd):/code is source:destination and $(pwd) maps to current directory where the script is called.
-#  -i   : interactive. 
-#  -t   : allocate a pseudo terminal
-#  windblade/ikemen-dev:latest        : docker image configured with the tooling required to build the binaries.
-#  bash -c 'cd /code && bash -x get.sh' : command called when the container launches. In changes to the code directory
-#  then execute both get and build scripts 
-
 cd ..
 
 if [ ! -d ./bin ]; then
