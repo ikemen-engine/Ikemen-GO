9093db5f60f6468e4ed3099f8f0dd86ce8e1d7b7
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2021-05-27 15:16:34 -0500

    Update appveyor_build_step2.sh

--- build/appveyor_build_step2.sh
+++ build/appveyor_build_step2.sh
@@ -2,6 +2,9 @@
 cd ..
 export CGO_ENABLED=1
 
+rm 'src/Ikemen_Cylia_x64.syso'
+rm 'src/Ikemen_Cylia_x86.syso'
+
 echo "------------------------------------------------------------"
 echo "Building linux binary..."
 echo "------------------------------------------------------------"
@@ -18,16 +21,20 @@ go build -i -o ./bin/Ikemen_GO_mac ./src
 echo "------------------------------------------------------------"
 echo "Building windows x64 binary..."
 echo "------------------------------------------------------------"
+cp 'windres/Ikemen_Cylia_x64.syso' 'src/Ikemen_Cylia_x64.syso'
 export GOOS=windows
 export CC=x86_64-w64-mingw32-gcc
 export CXX=x86_64-w64-mingw32-g++
 go build -i -ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x64.exe ./src
+rm 'src/Ikemen_Cylia_x64.syso'
 
 echo "------------------------------------------------------------"
 echo "Building windows x86 binary..."
 echo "------------------------------------------------------------"
+cp 'windres/Ikemen_Cylia_x86.syso' 'src/Ikemen_Cylia_x86.syso'
 export GOOS=windows
 export GOARCH=386
 export CC=i686-w64-mingw32-gcc
 export CXX=i686-w64-mingw32-g++
 go build -i -ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x86.exe ./src
+rm 'src/Ikemen_Cylia_x86.syso'
