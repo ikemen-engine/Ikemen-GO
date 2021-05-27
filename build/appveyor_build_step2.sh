8eedee370c5195c39d7a05816b44d778eac80e73
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2021-05-27 11:16:19 -0500

    Update appveyor_build_step2.sh

--- build/appveyor_build_step2.sh
+++ build/appveyor_build_step2.sh
@@ -5,7 +5,7 @@ export CGO_ENABLED=1
 echo "------------------------------------------------------------"
 echo "Building linux binary..."
 echo "------------------------------------------------------------"
-go build -i -tags al_cmpt -o ./bin/Ikemen_GO_linux ./src
+go build -i -o ./bin/Ikemen_GO_linux ./src
 
 echo "------------------------------------------------------------"
 echo "Building mac binary..."
@@ -13,7 +13,7 @@ echo "------------------------------------------------------------"
 export GOOS=darwin
 export CC=o64-clang 
 export CXX=o64-clang++
-go build -i -tags al_cmpt -o ./bin/Ikemen_GO_mac ./src
+go build -i -o ./bin/Ikemen_GO_mac ./src
 
 echo "------------------------------------------------------------"
 echo "Building windows x64 binary..."
@@ -21,7 +21,7 @@ echo "------------------------------------------------------------"
 export GOOS=windows
 export CC=x86_64-w64-mingw32-gcc
 export CXX=x86_64-w64-mingw32-g++
-go build -i -tags al_cmpt -ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x64.exe ./src
+go build -i-ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x64.exe ./src
 
 echo "------------------------------------------------------------"
 echo "Building windows x86 binary..."
@@ -30,4 +30,4 @@ export GOOS=windows
 export GOARCH=386
 export CC=i686-w64-mingw32-gcc
 export CXX=i686-w64-mingw32-g++
-go build -i -tags al_cmpt -ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x86.exe ./src
+go build -i-ldflags "-H windowsgui" -o ./bin/Ikemen_GO_Win_x86.exe ./src
