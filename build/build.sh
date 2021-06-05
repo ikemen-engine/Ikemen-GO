232c351e4d0444603e5205cb79d09495083e7b6f
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2021-06-05 18:49:17 -0500

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -92,21 +92,21 @@ function varLinux() {
 
 # Build functions.
 function build() {
-	echo "buildNormal"
-	echo "$binName"
-	#go build -o ./bin/$binName ./src
+	#echo "buildNormal"
+	#echo "$binName"
+	go build -o ./bin/$binName ./src
 }
 
 function buildAlt() {
-	echo "buildAlt"
-	echo "$binName"
-	#go build -tags al_cmpt -o ./bin/$binName ./src
+	#echo "buildAlt"
+	#echo "$binName"
+	go build -tags al_cmpt -o ./bin/$binName ./src
 }
 
 function buildWin() {
-	echo "buildWin"
-	echo "$binName"
-	#go build -ldflags "-H windowsgui" -o ./bin/$binName ./src
+	#echo "buildWin"
+	#echo "$binName"
+	go build -ldflags "-H windowsgui" -o ./bin/$binName ./src
 }
 
 # Determine the target OS.
