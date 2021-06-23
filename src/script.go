378d102a060960c84cafbb0b38c2500e762f619a
K4thos <piotrpit.uek@gmail.com>
2021-06-23 22:34:46 +0200

    fixed winnerteam() function
    https://github.com/Windblade-GR01/Ikemen-GO/issues/280

--- src/script.go
+++ src/script.go
@@ -3871,13 +3871,17 @@ func triggerFunctions(l *lua.LState) {
 	})
 	luaRegister(l, "winnerteam", func(*lua.LState) int {
 		var winp int32 = -1
-		if !sys.endMatch && (sys.winTeam >= 0 || sys.debugWC.roundState() >= 3) {
-			w1 := sys.wins[0] >= sys.matchWins[0]
-			w2 := sys.wins[1] >= sys.matchWins[1]
-			if w1 != w2 {
-				winp = Btoi(w1) + Btoi(w2)*2
-			} else {
-				winp = 0
+		if !sys.endMatch {
+			if sys.matchOver() && sys.roundOver() {
+				w1 := sys.wins[0] >= sys.matchWins[0]
+				w2 := sys.wins[1] >= sys.matchWins[1]
+				if w1 != w2 {
+					winp = Btoi(w1) + Btoi(w2)*2
+				} else {
+					winp = 0
+				}
+			} else if  sys.winTeam >= 0 || sys.debugWC.roundState() >= 3 {
+				winp = int32(sys.winTeam) + 1
 			}
 		}
 		l.Push(lua.LNumber(winp))
