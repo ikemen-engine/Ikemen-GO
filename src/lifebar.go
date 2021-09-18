ccefdf55f90f925a6718f21ea0b8835f61177012
K4thos <piotrpit.uek@gmail.com>
2021-09-18 15:26:37 +0200

    fixed lifebar win elements timing
    in mugen win.time counter doesn't seem to be delayed by over.hittime and over.wintime, only over.waittime affects it

--- src/lifebar.go
+++ src/lifebar.go
@@ -2038,7 +2038,8 @@ func (ro *LifeBarRound) act() bool {
 					ro.to_bg[i].Action()
 				}
 			}
-			if sys.intro < -(ro.over_hittime + ro.over_waittime + ro.over_wintime) {
+			//if sys.intro < -(ro.over_hittime + ro.over_waittime + ro.over_wintime) {
+			if sys.intro < -ro.over_waittime {
 				wt := sys.winTeam
 				if wt < 0 {
 					wt = 0
