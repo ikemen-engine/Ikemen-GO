62eb6da198d7f774177328b4e566cbd79fa84ce3
K4thos <piotrpit.uek@gmail.com>
2021-11-05 23:08:57 +0100

    added Hold Dir commands to common.cmd
    used by trainning.zss. Some characters don't have these commands declared despite kfm.cmd stating that they are "Required (do not remove)".
    
    reported here: https://github.com/Windblade-GR01/Ikemen-GO/issues/383

--- data/common.cmd
+++ data/common.cmd
@@ -62,3 +62,23 @@ time = 1
 name = "menu"
 command = m
 time = 1
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+
+[Command]
+name = "holdup"
+command = /$U
+time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
