ikemen-engine/Ikemen-GO

commit ccc6495809e093ea8d8c9214428f57113e505d9b

fixed screenpack scale parameters (font.scale => scale)
Incorrect naming convention. Based on my tests of every text parameter in mugen, '.font.scale' is not used even once but some elements did react to just '.scale' parameters (up to this point ikemen ignored them). Not all text elements in mugen support scaling, so this commit also adds missing "Ikemen feature" labels for some of these parameters.

external/script/main.lua
@@ -631,7 +631,7 @@ function main.f_createTextImg(t, prefix, mod)
 	local mod = mod or {}
 	if t[prefix .. '_font'] == nil then t[prefix .. '_font'] = {} end
 	if t[prefix .. '_offset'] == nil then t[prefix .. '_offset'] = {} end
-	if t[prefix .. '_font_scale'] == nil then t[prefix .. '_font_scale'] = {} end
+	if t[prefix .. '_scale'] == nil then t[prefix .. '_scale'] = {} end
 	return text:create({
 		font =   t[prefix .. '_font'][1],
 		bank =   t[prefix .. '_font'][2],
@@ -639,8 +639,8 @@ function main.f_createTextImg(t, prefix, mod)
 		text =   t[prefix .. '_text'],
 		x =      (t[prefix .. '_offset'][1] or 0) + (mod.x or 0),
 		y =      (t[prefix .. '_offset'][2] or 0) + (mod.y or 0),
-		scaleX = (t[prefix .. '_font_scale'][1] or 1) * (mod.scaleX or 1),
-		scaleY = (t[prefix .. '_font_scale'][2] or 1) * (mod.scaleY or 1),
+		scaleX = (t[prefix .. '_scale'][1] or 1) * (mod.scaleX or 1),
+		scaleY = (t[prefix .. '_scale'][2] or 1) * (mod.scaleY or 1),
 		r =      t[prefix .. '_font'][4],
 		g =      t[prefix .. '_font'][5],
 		b =      t[prefix .. '_font'][6],
@@ -3785,8 +3785,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].displayname,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_selected_active_font_scale[1],
-						scaleY = motif[section].menu_item_selected_active_font_scale[2],
+						scaleX = motif[section].menu_item_selected_active_scale[1],
+						scaleY = motif[section].menu_item_selected_active_scale[2],
 						r =      motif[section].menu_item_selected_active_font[4],
 						g =      motif[section].menu_item_selected_active_font[5],
 						b =      motif[section].menu_item_selected_active_font[6],
@@ -3802,8 +3802,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].displayname,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_active_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_active_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_active_font_scale[1],
-						scaleY = motif[section].menu_item_active_font_scale[2],
+						scaleX = motif[section].menu_item_active_scale[1],
+						scaleY = motif[section].menu_item_active_scale[2],
 						r =      motif[section].menu_item_active_font[4],
 						g =      motif[section].menu_item_active_font[5],
 						b =      motif[section].menu_item_active_font[6],
@@ -3820,8 +3820,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].vardisplay,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_value_active_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_value_active_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_value_active_font_scale[1],
-						scaleY = motif[section].menu_item_value_active_font_scale[2],
+						scaleX = motif[section].menu_item_value_active_scale[1],
+						scaleY = motif[section].menu_item_value_active_scale[2],
 						r =      motif[section].menu_item_value_active_font[4],
 						g =      motif[section].menu_item_value_active_font[5],
 						b =      motif[section].menu_item_value_active_font[6],
@@ -3845,8 +3845,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].displayname,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_selected_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_selected_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_selected_font_scale[1],
-						scaleY = motif[section].menu_item_selected_font_scale[2],
+						scaleX = motif[section].menu_item_selected_scale[1],
+						scaleY = motif[section].menu_item_selected_scale[2],
 						r =      motif[section].menu_item_selected_font[4],
 						g =      motif[section].menu_item_selected_font[5],
 						b =      motif[section].menu_item_selected_font[6],
@@ -3862,8 +3862,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].displayname,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_font_scale[1],
-						scaleY = motif[section].menu_item_font_scale[2],
+						scaleX = motif[section].menu_item_scale[1],
+						scaleY = motif[section].menu_item_scale[2],
 						r =      motif[section].menu_item_font[4],
 						g =      motif[section].menu_item_font[5],
 						b =      motif[section].menu_item_font[6],
@@ -3880,8 +3880,8 @@ function main.f_menuCommonDraw(t, item, cursorPosY, moveTxt, section, bgdef, tit
 						text =   t[i].vardisplay,
 						x =      motif[section].menu_pos[1] + motif[section].menu_item_value_offset[1] + (i - 1) * motif[section].menu_item_spacing[1],
 						y =      motif[section].menu_pos[2] + motif[section].menu_item_value_offset[2] + (i - 1) * motif[section].menu_item_spacing[2] - moveTxt,
-						scaleX = motif[section].menu_item_value_font_scale[1],
-						scaleY = motif[section].menu_item_value_font_scale[2],
+						scaleX = motif[section].menu_item_value_scale[1],
+						scaleY = motif[section].menu_item_value_scale[2],
 						r =      motif[section].menu_item_value_font[4],
 						g =      motif[section].menu_item_value_font[5],
 						b =      motif[section].menu_item_value_font[6],

external/script/options.lua
@@ -1933,8 +1933,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 			text =   motif.option_info.keymenu_itemname_playerno:gsub('%%i', tostring(i + player - side)),
 			x =      motif.option_info['keymenu_p' .. i .. '_pos'][1] + motif.option_info['keymenu_item_p' .. i .. '_offset'][1],
 			y =      motif.option_info['keymenu_p' .. i .. '_pos'][2] + motif.option_info['keymenu_item_p' .. i .. '_offset'][2],
-			scaleX = motif.option_info['keymenu_item_p' .. i .. '_font_scale'][1],
-			scaleY = motif.option_info['keymenu_item_p' .. i .. '_font_scale'][2],
+			scaleX = motif.option_info['keymenu_item_p' .. i .. '_scale'][1],
+			scaleY = motif.option_info['keymenu_item_p' .. i .. '_scale'][2],
 			r =      motif.option_info['keymenu_item_p' .. i .. '_font'][4],
 			g =      motif.option_info['keymenu_item_p' .. i .. '_font'][5],
 			b =      motif.option_info['keymenu_item_p' .. i .. '_font'][6],
@@ -1995,8 +1995,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 						text =   t[i].displayname,
 						x =      t_pos[j][1] + motif.option_info.keymenu_item_active_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 						y =      t_pos[j][2] + motif.option_info.keymenu_item_active_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-						scaleX = motif.option_info.keymenu_item_active_font_scale[1],
-						scaleY = motif.option_info.keymenu_item_active_font_scale[2],
+						scaleX = motif.option_info.keymenu_item_active_scale[1],
+						scaleY = motif.option_info.keymenu_item_active_scale[2],
 						r =      motif.option_info.keymenu_item_active_font[4],
 						g =      motif.option_info.keymenu_item_active_font[5],
 						b =      motif.option_info.keymenu_item_active_font[6],
@@ -2014,8 +2014,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 								text =   t[i]['vardisplay' .. j + player - side],
 								x =      t_pos[j][1] + motif.option_info.keymenu_item_value_conflict_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 								y =      t_pos[j][2] + motif.option_info.keymenu_item_value_conflict_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-								scaleX = motif.option_info.keymenu_item_value_conflict_font_scale[1],
-								scaleY = motif.option_info.keymenu_item_value_conflict_font_scale[2],
+								scaleX = motif.option_info.keymenu_item_value_conflict_scale[1],
+								scaleY = motif.option_info.keymenu_item_value_conflict_scale[2],
 								r =      motif.option_info.keymenu_item_value_conflict_font[4],
 								g =      motif.option_info.keymenu_item_value_conflict_font[5],
 								b =      motif.option_info.keymenu_item_value_conflict_font[6],
@@ -2032,8 +2032,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 								text =   t[i]['vardisplay' .. j + player - side],
 								x =      t_pos[j][1] + motif.option_info.keymenu_item_value_active_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 								y =      t_pos[j][2] + motif.option_info.keymenu_item_value_active_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-								scaleX = motif.option_info.keymenu_item_value_active_font_scale[1],
-								scaleY = motif.option_info.keymenu_item_value_active_font_scale[2],
+								scaleX = motif.option_info.keymenu_item_value_active_scale[1],
+								scaleY = motif.option_info.keymenu_item_value_active_scale[2],
 								r =      motif.option_info.keymenu_item_value_active_font[4],
 								g =      motif.option_info.keymenu_item_value_active_font[5],
 								b =      motif.option_info.keymenu_item_value_active_font[6],
@@ -2051,8 +2051,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 							text =   t[i].infodisplay,
 							x =      t_pos[j][1] + motif.option_info.keymenu_item_info_active_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 							y =      t_pos[j][2] + motif.option_info.keymenu_item_info_active_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-							scaleX = motif.option_info.keymenu_item_value_active_font_scale[1],
-							scaleY = motif.option_info.keymenu_item_value_active_font_scale[2],
+							scaleX = motif.option_info.keymenu_item_value_active_scale[1],
+							scaleY = motif.option_info.keymenu_item_value_active_scale[2],
 							r =      motif.option_info.keymenu_item_info_active_font[4],
 							g =      motif.option_info.keymenu_item_info_active_font[5],
 							b =      motif.option_info.keymenu_item_info_active_font[6],
@@ -2075,8 +2075,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 						text =   t[i].displayname,
 						x =      t_pos[j][1] + motif.option_info.keymenu_item_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 						y =      t_pos[j][2] + motif.option_info.keymenu_item_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-						scaleX = motif.option_info.keymenu_item_font_scale[1],
-						scaleY = motif.option_info.keymenu_item_font_scale[2],
+						scaleX = motif.option_info.keymenu_item_scale[1],
+						scaleY = motif.option_info.keymenu_item_scale[2],
 						r =      motif.option_info.keymenu_item_font[4],
 						g =      motif.option_info.keymenu_item_font[5],
 						b =      motif.option_info.keymenu_item_font[6],
@@ -2094,8 +2094,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 								text =   t[i]['vardisplay' .. j + player - side],
 								x =      t_pos[j][1] + motif.option_info.keymenu_item_value_conflict_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 								y =      t_pos[j][2] + motif.option_info.keymenu_item_value_conflict_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-								scaleX = motif.option_info.keymenu_item_value_conflict_font_scale[1],
-								scaleY = motif.option_info.keymenu_item_value_conflict_font_scale[2],
+								scaleX = motif.option_info.keymenu_item_value_conflict_scale[1],
+								scaleY = motif.option_info.keymenu_item_value_conflict_scale[2],
 								r =      motif.option_info.keymenu_item_value_conflict_font[4],
 								g =      motif.option_info.keymenu_item_value_conflict_font[5],
 								b =      motif.option_info.keymenu_item_value_conflict_font[6],
@@ -2112,8 +2112,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 								text =   t[i]['vardisplay' .. j + player - side],
 								x =      t_pos[j][1] + motif.option_info.keymenu_item_value_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 								y =      t_pos[j][2] + motif.option_info.keymenu_item_value_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-								scaleX = motif.option_info.keymenu_item_value_font_scale[1],
-								scaleY = motif.option_info.keymenu_item_value_font_scale[2],
+								scaleX = motif.option_info.keymenu_item_value_scale[1],
+								scaleY = motif.option_info.keymenu_item_value_scale[2],
 								r =      motif.option_info.keymenu_item_value_font[4],
 								g =      motif.option_info.keymenu_item_value_font[5],
 								b =      motif.option_info.keymenu_item_value_font[6],
@@ -2131,8 +2131,8 @@ function options.f_keyCfg(cfgType, controller, bgdef, skipClear)
 							text =   t[i].infodisplay,
 							x =      t_pos[j][1] + motif.option_info.keymenu_item_info_offset[1] + (i - 1) * motif.option_info.keymenu_item_spacing[1],
 							y =      t_pos[j][2] + motif.option_info.keymenu_item_info_offset[2] + (i - 1) * motif.option_info.keymenu_item_spacing[2],
-							scaleX = motif.option_info.keymenu_item_value_active_font_scale[1],
-							scaleY = motif.option_info.keymenu_item_value_active_font_scale[2],
+							scaleX = motif.option_info.keymenu_item_value_active_scale[1],
+							scaleY = motif.option_info.keymenu_item_value_active_scale[2],
 							r =      motif.option_info.keymenu_item_info_font[4],
 							g =      motif.option_info.keymenu_item_info_font[5],
 							b =      motif.option_info.keymenu_item_info_font[6],

external/script/start.lua
@@ -2102,8 +2102,8 @@ function start.f_selectScreen()
 							text =   name,
 							x =      motif.select_info['p' .. side .. '_name_offset'][1] + (i - 1) * motif.select_info['p' .. side .. '_name_spacing'][1],
 							y =      motif.select_info['p' .. side .. '_name_offset'][2] + (i - 1) * motif.select_info['p' .. side .. '_name_spacing'][2],
-							scaleX = motif.select_info['p' .. side .. '_name_font_scale'][1],
-							scaleY = motif.select_info['p' .. side .. '_name_font_scale'][2],
+							scaleX = motif.select_info['p' .. side .. '_name_scale'][1],
+							scaleY = motif.select_info['p' .. side .. '_name_scale'][2],
 							r =      motif.select_info['p' .. side .. '_name_font'][4],
 							g =      motif.select_info['p' .. side .. '_name_font'][5],
 							b =      motif.select_info['p' .. side .. '_name_font'][6],
@@ -2173,8 +2173,8 @@ function start.f_selectScreen()
 						text =   t_txt[i],
 						x =      motif.select_info.stage_pos[1] + motif.select_info[stageActiveType .. '_offset'][1],
 						y =      motif.select_info.stage_pos[2] + motif.select_info[stageActiveType .. '_offset'][2] + main.f_ySpacing(motif.select_info, stageActiveType .. '_font') * (i - 1),
-						scaleX = motif.select_info[stageActiveType .. '_font_scale'][1],
-						scaleY = motif.select_info[stageActiveType .. '_font_scale'][2],
+						scaleX = motif.select_info[stageActiveType .. '_scale'][1],
+						scaleY = motif.select_info[stageActiveType .. '_scale'][2],
 						r =      motif.select_info[stageActiveType .. '_font'][4],
 						g =      motif.select_info[stageActiveType .. '_font'][5],
 						b =      motif.select_info[stageActiveType .. '_font'][6],
@@ -2367,8 +2367,8 @@ function start.f_teamMenu(side, t)
 					text =   t[i].displayname,
 					x =      motif.select_info['p' .. side .. '_teammenu_pos'][1] + motif.select_info['p' .. side .. '_teammenu_item_offset'][1] + motif.select_info['p' .. side .. '_teammenu_item_spacing'][1] * (i - 1),
 					y =      motif.select_info['p' .. side .. '_teammenu_pos'][2] + motif.select_info['p' .. side .. '_teammenu_item_offset'][2] + motif.select_info['p' .. side .. '_teammenu_item_spacing'][2] * (i - 1),
-					scaleX = motif.select_info[t_teamActiveType[side] .. '_font_scale'][1],
-					scaleY = motif.select_info[t_teamActiveType[side] .. '_font_scale'][2],
+					scaleX = motif.select_info[t_teamActiveType[side] .. '_scale'][1],
+					scaleY = motif.select_info[t_teamActiveType[side] .. '_scale'][2],
 					r =      motif.select_info[t_teamActiveType[side] .. '_font'][4],
 					g =      motif.select_info[t_teamActiveType[side] .. '_font'][5],
 					b =      motif.select_info[t_teamActiveType[side] .. '_font'][6],
@@ -2386,8 +2386,8 @@ function start.f_teamMenu(side, t)
 					text =   t[i].displayname,
 					x =      motif.select_info['p' .. side .. '_teammenu_pos'][1] + motif.select_info['p' .. side .. '_teammenu_item_offset'][1] + motif.select_info['p' .. side .. '_teammenu_item_spacing'][1] * (i - 1),
 					y =      motif.select_info['p' .. side .. '_teammenu_pos'][2] + motif.select_info['p' .. side .. '_teammenu_item_offset'][2] + motif.select_info['p' .. side .. '_teammenu_item_spacing'][2] * (i - 1),
-					scaleX = motif.select_info['p' .. side .. '_teammenu_item_font_scale'][1],
-					scaleY = motif.select_info['p' .. side .. '_teammenu_item_font_scale'][2],
+					scaleX = motif.select_info['p' .. side .. '_teammenu_item_scale'][1],
+					scaleY = motif.select_info['p' .. side .. '_teammenu_item_scale'][2],
 					r =      motif.select_info['p' .. side .. '_teammenu_item_font'][4],
 					g =      motif.select_info['p' .. side .. '_teammenu_item_font'][5],
 					b =      motif.select_info['p' .. side .. '_teammenu_item_font'][6],
@@ -2865,8 +2865,8 @@ function start.f_selectVersus(active, t_orderSelect)
 						text =   start.f_getName(start.p[side].t_selTemp[i].ref, false, side),
 						x =      motif.vs_screen['p' .. side .. '_name_pos'][1] + motif.vs_screen['p' .. side .. '_name_offset'][1] + (i - 1) * motif.vs_screen['p' .. side .. '_name_spacing'][1],
 						y =      motif.vs_screen['p' .. side .. '_name_pos'][2] + motif.vs_screen['p' .. side .. '_name_offset'][2] + (i - 1) * motif.vs_screen['p' .. side .. '_name_spacing'][2],
-						scaleX = motif.vs_screen['p' .. side .. '_name_font_scale'][1],
-						scaleY = motif.vs_screen['p' .. side .. '_name_font_scale'][2],
+						scaleX = motif.vs_screen['p' .. side .. '_name_scale'][1],
+						scaleY = motif.vs_screen['p' .. side .. '_name_scale'][2],
 						r =      motif.vs_screen['p' .. side .. '_name_font'][4],
 						g =      motif.vs_screen['p' .. side .. '_name_font'][5],
 						b =      motif.vs_screen['p' .. side .. '_name_font'][6],
@@ -3137,7 +3137,7 @@ end
 --;===========================================================
 --; VICTORY SCREEN
 --;===========================================================
-local txt_winquote = main.f_createTextImg(motif.victory_screen, 'winquote', {scaleX = motif.victory_screen.winquote_scale[1], scaleY = motif.victory_screen.winquote_scale[2]})
+local txt_winquote = main.f_createTextImg(motif.victory_screen, 'winquote')
 local overlay_winquote = main.f_createOverlay(motif.victory_screen, 'overlay')
 local t_txt_winquoteName = {}
 for i = 1, 2 do
@@ -3590,8 +3590,8 @@ function start.f_continue()
 				text =   motif.continue_screen[var .. '_text'],
 				x =      motif.continue_screen.pos[1] + motif.continue_screen[var .. '_offset'][1],
 				y =      motif.continue_screen.pos[2] + motif.continue_screen[var .. '_offset'][2],
-				scaleX = motif.continue_screen[var .. '_font_scale'][1],
-				scaleY = motif.continue_screen[var .. '_font_scale'][2],
+				scaleX = motif.continue_screen[var .. '_scale'][1],
+				scaleY = motif.continue_screen[var .. '_scale'][2],
 				r =      motif.continue_screen[var .. '_font'][4],
 				g =      motif.continue_screen[var .. '_font'][5],
 				b =      motif.continue_screen[var .. '_font'][6],

external/script/menu.lua
@@ -653,8 +653,8 @@ function menu.f_commandlistRender(section, t)
 				local font_def = main.font_def[motif[section].movelist_text_font[1] .. motif[section].movelist_text_font[7]]
 				--render glyph
 				if v.glyph and motif.glyphs_data[v.text] ~= nil then
-					local scaleX = font_def.Size[2] * motif[section].movelist_text_font_scale[2] / motif.glyphs_data[v.text].info.Size[2] * motif[section].movelist_glyphs_scale[1]
-					local scaleY = font_def.Size[2] * motif[section].movelist_text_font_scale[2] / motif.glyphs_data[v.text].info.Size[2] * motif[section].movelist_glyphs_scale[2]
+					local scaleX = font_def.Size[2] * motif[section].movelist_text_scale[2] / motif.glyphs_data[v.text].info.Size[2] * motif[section].movelist_glyphs_scale[1]
+					local scaleY = font_def.Size[2] * motif[section].movelist_text_scale[2] / motif.glyphs_data[v.text].info.Size[2] * motif[section].movelist_glyphs_scale[2]
 					if v.align == -1 then
 						alignOffset = alignOffset - motif.glyphs_data[v.text].info.Size[1] * scaleX
 					end
@@ -691,7 +691,7 @@ function menu.f_commandlistRender(section, t)
 					})
 					data:draw()
 					if k < #cmdList[n] then
-						width = fontGetTextWidth(main.font[data.font .. data.height], v.text, data.bank) * motif[section].movelist_text_font_scale[1] + motif[section].movelist_text_spacing[1]
+						width = fontGetTextWidth(main.font[data.font .. data.height], v.text, data.bank) * motif[section].movelist_text_scale[1] + motif[section].movelist_text_spacing[1]
 					end
 				end
 				if v.align == 0 then

external/script/storyboard.lua
@@ -222,7 +222,7 @@ local function f_parse(path)
 								anim = -1,
 								text = '',
 								font = {'f-6x9.def', 0, 0, 255, 255, 255, -1},
-								font_scale = {1.0, 1.0}, --Ikemen feature
+								scale = {1.0, 1.0}, --Ikemen feature
 								palfx_time = -1, --Ikemen feature
 								palfx_add = {0, 0, 0}, --Ikemen feature
 								palfx_mul = {256, 256, 256}, --Ikemen feature
@@ -407,8 +407,8 @@ local function f_parse(path)
 					text =   t_layer[k2].text,
 					x =      t.scene[k].layerall_pos[1] + t_layer[k2].offset[1],
 					y =      t.scene[k].layerall_pos[2] + t_layer[k2].offset[2],
-					scaleX = t_layer[k2].font_scale[1],
-					scaleY = t_layer[k2].font_scale[2],
+					scaleX = t_layer[k2].scale[1],
+					scaleY = t_layer[k2].scale[2],
 					r =      t_layer[k2].font[4],
 					g =      t_layer[k2].font[5],
 					b =      t_layer[k2].font[6],

commit cb24dc92c29b7205ff966734ddf5fde1e96046b4

f_ySpacing regression
required by: https://github.com/K4thos/Ikemen_GO/commit/ccc6495809e093ea8d8c9214428f57113e505d9b

external/script/main.lua
@@ -1199,7 +1199,7 @@ end
 
 --y spacing calculation
 function main.f_ySpacing(t, key)
-	local font_def = main.font_def[t[key][1] .. t[key][7]]
+	local font_def = main.font_def[t[key .. '_font'][1] .. t[key .. '_font'][7]]
 	if font_def == nil then return 0 end
 	return main.f_round(font_def.Size[2] * t[key .. '_scale'][2] + font_def.Spacing[2] * t[key .. '_scale'][2])
 end
@@ -1556,7 +1556,7 @@ function main.f_warning(t, background, info, title, txt, overlay)
 		for i = 1, #t do
 			txt:update({
 				text = t[i],
-				y = info.text_offset[2] + main.f_ySpacing(info, 'text_font') * (i - 1),
+				y = info.text_offset[2] + main.f_ySpacing(info, 'text') * (i - 1),
 			})
 			txt:draw()
 		end
@@ -2462,7 +2462,7 @@ main.t_itemname = {
 			txt_textinput,
 			overlay_textinput,
 			motif[main.group].textinput_offset[2],
-			main.f_ySpacing(motif.title_info, 'textinput_font'),
+			main.f_ySpacing(motif.title_info, 'textinput'),
 			motif[main.background]
 		)
 		if name ~= '' then
@@ -2472,7 +2472,7 @@ main.t_itemname = {
 				txt_textinput,
 				overlay_textinput,
 				motif[main.group].textinput_offset[2],
-				main.f_ySpacing(motif.title_info, 'textinput_font'),
+				main.f_ySpacing(motif.title_info, 'textinput'),
 				motif[main.background]
 			)
 			if address:match('^[0-9%.]+$') then
@@ -3317,7 +3317,7 @@ function main.f_connect(server, t)
 		for i = 1, #t do
 			txt_connecting:update({
 				text = t[i],
-				y = motif[main.group].connecting_offset[2] + main.f_ySpacing(motif.title_info, 'connecting_font') * (i - 1),
+				y = motif[main.group].connecting_offset[2] + main.f_ySpacing(motif.title_info, 'connecting') * (i - 1),
 			})
 			txt_connecting:draw()
 		end

external/script/options.lua
@@ -80,7 +80,7 @@ options.t_itemname = {
 				txt_textinput,
 				overlay_textinput,
 				motif.option_info.textinput_offset[2],
-				main.f_ySpacing(motif.option_info, 'textinput_font'),
+				main.f_ySpacing(motif.option_info, 'textinput'),
 				motif.optionbgdef
 			)
 			if tonumber(port) ~= nil then
@@ -781,7 +781,7 @@ options.t_itemname = {
 					txt_textinput,
 					overlay_textinput,
 					motif.option_info.textinput_offset[2],
-					main.f_ySpacing(motif.option_info, 'textinput_font'),
+					main.f_ySpacing(motif.option_info, 'textinput'),
 					motif.optionbgdef
 				))
 			if width ~= nil then
@@ -791,7 +791,7 @@ options.t_itemname = {
 					txt_textinput,
 					overlay_textinput,
 					motif.option_info.textinput_offset[2],
-					main.f_ySpacing(motif.option_info, 'textinput_font'),
+					main.f_ySpacing(motif.option_info, 'textinput'),
 					motif.optionbgdef
 				))
 				if height ~= nil then

external/script/start.lua
@@ -2172,7 +2172,7 @@ function start.f_selectScreen()
 						align =  motif.select_info[stageActiveType .. '_font'][3],
 						text =   t_txt[i],
 						x =      motif.select_info.stage_pos[1] + motif.select_info[stageActiveType .. '_offset'][1],
-						y =      motif.select_info.stage_pos[2] + motif.select_info[stageActiveType .. '_offset'][2] + main.f_ySpacing(motif.select_info, stageActiveType .. '_font') * (i - 1),
+						y =      motif.select_info.stage_pos[2] + motif.select_info[stageActiveType .. '_offset'][2] + main.f_ySpacing(motif.select_info, stageActiveType) * (i - 1),
 						scaleX = motif.select_info[stageActiveType .. '_scale'][1],
 						scaleY = motif.select_info[stageActiveType .. '_scale'][2],
 						r =      motif.select_info[stageActiveType .. '_font'][4],
@@ -2192,7 +2192,7 @@ function start.f_selectScreen()
 		for i = 1, #t_recordText do
 			txt_recordSelect:update({
 				text = t_recordText[i],
-				y = motif.select_info.record_offset[2] + main.f_ySpacing(motif.select_info, 'record_font') * (i - 1),
+				y = motif.select_info.record_offset[2] + main.f_ySpacing(motif.select_info, 'record') * (i - 1),
 			})
 			txt_recordSelect:draw()
 		end
@@ -2951,7 +2951,7 @@ local function f_drawTextAtLayerNo(t, prefix, t_text, txt, layerNo)
 	for i = 1, #t_text do
 		txt:update({
 			text = t_text[i],
-			y =    t[prefix .. '_offset'][2] + main.f_ySpacing(t, prefix .. '_font') * (i - 1),
+			y =    t[prefix .. '_offset'][2] + main.f_ySpacing(t, prefix) * (i - 1),
 		})
 		txt:draw()
 	end