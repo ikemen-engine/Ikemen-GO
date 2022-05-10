1f7df7117c2326fdbb59813c507d992c5ef60d0d
Sam Hocevar <sam@hocevar.net>
2022-05-10 16:25:07 +0200

    Move core TTF loading code to a platform-specific file
    This allows to disable TTF loading when targeting JavaScript, which does not
    have the findfont or glfont libraries.

--- src/util_js.go
+++ src/util_js.go
@@ -6,6 +6,7 @@ import (
 	"syscall/js"
 )
 
+// Message box implementation using basic JavaScript alert()
 var alert = js.Global().Get("alert")
 
 func ShowInfoDialog(message, title string) {
@@ -14,4 +15,9 @@ func ShowInfoDialog(message, title string) {
 
 func ShowErrorDialog(message string) {
 	alert.Invoke("I.K.E.M.E.N Error\n\n" + message)
+}
+
+// TTF font loading stub
+func LoadFntTtf(f *Fnt, fontfile string, filename string, height int32) {
+	panic(Error("TrueType fonts are not supported on this platform"))
 }
