ikemen-engine/Ikemen-GO

commit 0a9d30b3cd3101b0283d418371a9a04a15ddde5d

Move GL buffer clearing to render_gl.go

src/render_gl.go
@@ -319,6 +319,8 @@ func (r *Renderer) IsOpenGL() bool {
 
 func (r *Renderer) BeginFrame() {
 	gl.BindFramebuffer(gl.FRAMEBUFFER, r.fbo)
+	gl.Viewport(0, 0, int(sys.scrrect[2]), int(sys.scrrect[3]))
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 }
 
 func (r *Renderer) EndFrame() {

src/system.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
-	gl "github.com/fyne-io/gl-js"
 	glfw "github.com/fyne-io/glfw-js"
 	lua "github.com/yuin/gopher-lua"
 )
@@ -491,6 +490,7 @@ func (s *System) runMainThreadTask() {
 		}
 	}
 }
+
 func (s *System) await(fps int) bool {
 	if !s.frameSkip {
 		// Render the finished frame
@@ -520,16 +520,9 @@ func (s *System) await(fps int) bool {
 		s.frameSkip = true
 	}
 	s.eventUpdate()
-	if !s.frameSkip {
-		//var width, height = glfw.GetCurrentContext().GetFramebufferSize()
-		//gl.Viewport(0, 0, int32(width), int32(height))
-		gl.Viewport(0, 0, int(s.scrrect[2]), int(s.scrrect[3]))
-		if s.netInput == nil {
-			gl.Clear(gl.COLOR_BUFFER_BIT)
-		}
-	}
 	return !s.gameEnd
 }
+
 func (s *System) update() bool {
 	s.frameCounter++
 	if s.fileInput != nil {

commit bab71bc218f34fffb44969efacb7b2b5b1bd872c

Fix screen flickering in netplay matches
This was a problem in some graphic cards. The issue was introduced in 0a9d30b3cd3101b0283d418371a9a04a15ddde5d.

This commit doesn't fix the sprite flickering in select screen or main menu.

src/render_gl.go
@@ -282,7 +282,9 @@ func (r *Renderer) Close() {
 func (r *Renderer) BeginFrame() {
 	gl.BindFramebuffer(gl.FRAMEBUFFER, r.fbo)
 	gl.Viewport(0, 0, int(sys.scrrect[2]), int(sys.scrrect[3]))
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	if sys.netInput == nil {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	}
 }
 
 func (r *Renderer) EndFrame() {