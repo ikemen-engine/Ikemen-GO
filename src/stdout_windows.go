8604d22dc51202491c1d3232ae5be5026e93112d
K4thos <piotrpit.uek@gmail.com>
2023-03-10 03:53:00 +0100

    build: simplify releases workflow

--- src/stdout_windows.go
+++ src/stdout_windows.go
@@ -31,5 +31,6 @@ func init() {
 	os.Stderr = os.NewFile(uintptr(herr), "/dev/stderr")
 	log.SetOutput(os.Stderr)
 	log.Println("Ikemen, GO!")
+	log.Println(Version, BuildTime)
 	runtime.LockOSThread()
 }
