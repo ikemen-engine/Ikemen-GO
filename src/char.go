ikemen-engine/Ikemen-GO

commit 377e9e9df8055686dbb5172702cbefc783699862

fix: make round end closer to Mugen's behavior
- Ctrl is set to 0 after wait.time
- CtrlOver flag behavior changed. It no longer checks for which phase of the round end we are in, but rather it is toggled on when the previous point happens
- CtrlOver boolean in the source code renamed to InputOver, as it is more descriptive of what it actually does
- Refactor: disabling inputs after wait.time is more simple: the character is directly flagged with NoInput (unless PostRoundInput is active)
- Sending characters to their win/lose poses leaves ctrl the way it was instead of changing it to 1

- A side effect of these changes is that the applications of the PostRoundInput flag have been reduced, as that is mostly the new default behavior (like Mugen). This flag can still be used to allow inputs just before the win poses

- Basic actions like walking moved back to "action prepare", mostly for compatibility. Guarding instructions are still processed after current state, as that's what is actually responsible for fixing previous bugs

src/char.go
@@ -18,6 +18,7 @@ const (
 	SCF_ctrl
 	SCF_standby
 	SCF_guard
+	SCF_ctrlwait
 	SCF_over
 	SCF_ko_round_middle
 	SCF_dizzy
@@ -4672,15 +4673,11 @@ func (c *Char) hitPause() bool {
 func (c *Char) angleSet(a float32) {
 	c.angle = a
 }
-func (c *Char) ctrlOver() bool {
-	return !c.alive() || sys.time == 0 ||
-		(sys.intro <= -(sys.lifebar.ro.over_hittime+sys.lifebar.ro.over_waittime) &&
-			sys.intro > -(sys.lifebar.ro.over_hittime+sys.lifebar.ro.over_waittime+sys.lifebar.ro.over_wintime)) ||
-		sys.intro <= -sys.lifebar.ro.over_time
+func (c *Char) inputOver() bool {
+	return !c.alive() || sys.time == 0 || sys.intro <= -sys.lifebar.ro.over_time
 }
 func (c *Char) over() bool {
-	return c.scf(SCF_over) || (c.ctrlOver() && (c.scf(SCF_ctrl) || c.ss.no == 5150) &&
-		c.ss.stateType != ST_A && c.ss.physics != ST_A)
+	return c.scf(SCF_over) || c.ss.no == 5150
 }
 func (c *Char) makeDust(x, y float32) {
 	if e, i := c.newExplod(); e != nil {
@@ -5454,10 +5451,71 @@ func (c *Char) actionPrepare() {
 	}
 	c.acttmp = -int8(Btoi(c.pauseBool)) * 2
 	if !c.pauseBool {
+		// Perform basic actions
+		if c.keyctrl[0] && c.cmd != nil {
+			if c.ctrl() && !c.inputOver() && (c.key >= 0 || c.helperIndex == 0) {
+				if !c.sf(CSF_nohardcodedkeys) {
+					// TODO disable jumps right after KO instead of after over.hittime
+					if !c.sf(CSF_nojump) && (!sys.roundEnd() || c.sf(CSF_postroundinput)) && c.ss.stateType == ST_S && c.cmd[0].Buffer.U > 0 {
+						if c.ss.no != 40 {
+							c.changeState(40, -1, -1, "")
+						}
+					} else if !c.sf(CSF_noairjump) && c.ss.stateType == ST_A && c.cmd[0].Buffer.Ub == 1 &&
+						c.pos[1] <= float32(c.gi().movement.airjump.height) &&
+						c.airJumpCount < c.gi().movement.airjump.num {
+						if c.ss.no != 45 || c.ss.time > 0 {
+							c.airJumpCount++
+							c.changeState(45, -1, -1, "")
+						}
+					} else {
+						if !c.sf(CSF_nocrouch) && c.ss.stateType == ST_S && c.cmd[0].Buffer.D > 0 {
+							if c.ss.no != 10 {
+								if c.ss.no != 100 {
+									c.vel[0] = 0
+								}
+								c.changeState(10, -1, -1, "")
+							}
+						} else if !c.sf(CSF_nostand) && c.ss.stateType == ST_C && c.cmd[0].Buffer.D < 0 {
+							if c.ss.no != 12 {
+								c.changeState(12, -1, -1, "")
+							}
+						} else if !c.sf(CSF_nowalk) && c.ss.stateType == ST_S &&
+							(c.cmd[0].Buffer.F > 0 || !(c.inguarddist && c.scf(SCF_guard)) &&
+								c.cmd[0].Buffer.B > 0) {
+							if c.ss.no != 20 {
+								c.changeState(20, -1, -1, "")
+							}
+						} else if !c.sf(CSF_nobrake) && c.ss.no == 20 &&
+							c.cmd[0].Buffer.B < 0 && c.cmd[0].Buffer.F < 0 {
+							c.changeState(0, -1, -1, "")
+						}
+						if c.inguarddist && c.scf(SCF_guard) && c.cmd[0].Buffer.B > 0 &&
+							!c.inGuardState() {
+							c.changeState(120, -1, -1, "")
+						}
+					}
+				}
+			} else {
+				switch c.ss.no {
+				case 11:
+					if !c.sf(CSF_nostand) {
+						c.changeState(12, -1, -1, "")
+					}
+				case 20:
+					if !c.sf(CSF_nobrake) && c.cmd[0].Buffer.U < 0 && c.cmd[0].Buffer.D < 0 &&
+						c.cmd[0].Buffer.B < 0 && c.cmd[0].Buffer.F < 0 {
+						c.changeState(0, -1, -1, "")
+					}
+				}
+			}
+		}
+		if c.ss.stateType != ST_A {
+			c.airJumpCount = 0
+		}
 		if !c.hitPause() {
 			if !sys.roundEnd() {
 				if c.alive() && c.life > 0 {
-					c.unsetSCF(SCF_over | SCF_ko_round_middle)
+					c.unsetSCF(SCF_over | SCF_ctrlwait | SCF_ko_round_middle)
 				}
 				if c.ss.no == 5150 || c.scf(SCF_over) {
 					c.setSCF(SCF_ko_round_middle)
@@ -5541,13 +5599,12 @@ func (c *Char) actionRun() {
 		c.stateChange2()
 		c.minus = 0
 		c.ss.sb.run(c)
-	}
-	if !c.pauseBool {
+		// Guarding instructions
 		c.unsetSCF(SCF_guard)
 		if sys.autoguard[c.playerNo] {
 			c.setSF(CSF_autoguard)
 		}
-		if !c.ctrlOver() &&
+		if !c.inputOver() &&
 			((c.scf(SCF_ctrl) || c.ss.no == 52) &&
 				c.ss.moveType == MT_I || c.inGuardState()) && c.cmd != nil &&
 			(c.cmd[0].Buffer.B > 0 || c.sf(CSF_autoguard)) &&
@@ -5556,64 +5613,9 @@ func (c *Char) actionRun() {
 				c.ss.stateType == ST_A && !c.sf(CSF_noairguard)) {
 			c.setSCF(SCF_guard)
 		}
-		if c.keyctrl[0] && c.cmd != nil {
-			if c.ctrl() && !c.ctrlOver() && (c.key >= 0 || c.helperIndex == 0) {
-				if !c.sf(CSF_nohardcodedkeys) {
-					if !c.sf(CSF_nojump) && (!sys.roundEnd() || c.sf(CSF_postroundinput)) && c.ss.stateType == ST_S && c.cmd[0].Buffer.U > 0 {
-						if c.ss.no != 40 {
-							c.changeState(40, -1, -1, "")
-						}
-					} else if !c.sf(CSF_noairjump) && c.ss.stateType == ST_A && c.cmd[0].Buffer.Ub == 1 &&
-						c.pos[1] <= float32(c.gi().movement.airjump.height) &&
-						c.airJumpCount < c.gi().movement.airjump.num {
-						if c.ss.no != 45 || c.ss.time > 0 {
-							c.airJumpCount++
-							c.changeState(45, -1, -1, "")
-						}
-					} else {
-						if !c.sf(CSF_nocrouch) && c.ss.stateType == ST_S && c.cmd[0].Buffer.D > 0 {
-							if c.ss.no != 10 {
-								if c.ss.no != 100 {
-									c.vel[0] = 0
-								}
-								c.changeState(10, -1, -1, "")
-							}
-						} else if !c.sf(CSF_nostand) && c.ss.stateType == ST_C && c.cmd[0].Buffer.D < 0 {
-							if c.ss.no != 12 {
-								c.changeState(12, -1, -1, "")
-							}
-						} else if !c.sf(CSF_nowalk) && c.ss.stateType == ST_S &&
-							(c.cmd[0].Buffer.F > 0 || !(c.inguarddist && c.scf(SCF_guard)) &&
-								c.cmd[0].Buffer.B > 0) {
-							if c.ss.no != 20 {
-								c.changeState(20, -1, -1, "")
-							}
-						} else if !c.sf(CSF_nobrake) && c.ss.no == 20 &&
-							c.cmd[0].Buffer.B < 0 && c.cmd[0].Buffer.F < 0 {
-							c.changeState(0, -1, -1, "")
-						}
-						if c.inguarddist && c.scf(SCF_guard) && c.cmd[0].Buffer.B > 0 &&
-							!c.inGuardState() {
-							c.changeState(120, -1, -1, "")
-						}
-					}
-				}
-			} else {
-				switch c.ss.no {
-				case 11:
-					if !c.sf(CSF_nostand) {
-						c.changeState(12, -1, -1, "")
-					}
-				case 20:
-					if !c.sf(CSF_nobrake) && c.cmd[0].Buffer.U < 0 && c.cmd[0].Buffer.D < 0 &&
-						c.cmd[0].Buffer.B < 0 && c.cmd[0].Buffer.F < 0 {
-						c.changeState(0, -1, -1, "")
-					}
-				}
-			}
-		}
-		if c.ss.stateType != ST_A {
-			c.airJumpCount = 0
+		if c.inguarddist && c.scf(SCF_guard) && c.cmd[0].Buffer.B > 0 &&
+			!c.inGuardState() {
+			c.changeState(120, -1, -1, "")
 		}
 	}
 	if sb, ok := c.gi().states[-10]; ok { // still minus 0
@@ -6599,7 +6601,7 @@ func (cl *CharList) clsn(getter *Char, proj bool) {
 				} else if getter.bindToId == c.id {
 					getter.setBindTime(0)
 				}
-				// This compensates for characters being able to act one frame sooner in Ikemen than in Mugen
+				// This compensates for characters being able to guard one frame sooner in Ikemen than in Mugen
 				if c.stCgi().ikemenver[0] == 0 && c.stCgi().ikemenver[1] == 0 {
 					ghv.hittime += 1
 					ghv.ctrltime += 1

src/system.go
@@ -913,8 +913,12 @@ func (s *System) commandUpdate() {
 				(r.ss.no == 0 || r.ss.no == 11 || r.ss.no == 20 || r.ss.no == 52) {
 				r.turn()
 			}
-			if (r.ctrlOver() && !r.sf(CSF_postroundinput)) || r.sf(CSF_noinput) ||
-				(r.aiLevel() > 0 && !r.alive()) {
+			if !r.sf(CSF_postroundinput) &&
+				s.intro <= -(s.lifebar.ro.over_hittime+s.lifebar.ro.over_waittime) &&
+				s.intro > -(s.lifebar.ro.over_hittime+s.lifebar.ro.over_waittime+s.lifebar.ro.over_wintime) {
+				r.setSF(CSF_noinput)
+			}
+			if r.inputOver() || r.sf(CSF_noinput) || (r.aiLevel() > 0 && !r.alive()) {
 				for j := range r.cmd {
 					r.cmd[j].BufReset()
 				}
@@ -1345,11 +1349,19 @@ func (s *System) action() {
 			}
 			rs4t := -(s.lifebar.ro.over_hittime + s.lifebar.ro.over_waittime)
 			if s.winskipped || s.intro >= rs4t-s.lifebar.ro.over_wintime {
-				if s.intro == rs4t {
-					if s.waitdown > 0 {
+				if s.waitdown > 0 {
+					if s.intro == rs4t - 1 {
 						for _, p := range s.chars {
-							if len(p) > 0 && !p[0].over() {
-								s.intro = rs4t + 1
+							if len(p) > 0 {
+								// Disable ctrl (once) and set "ctrl wait" flag
+								if p[0].scf(SCF_ctrl) && !p[0].scf(SCF_ctrlwait) && p[0].ss.stateType != ST_A && p[0].ss.stateType != ST_L {
+									p[0].setCtrl(false)
+									p[0].setSCF(SCF_ctrlwait)
+								}
+								// Freeze timer if any character is not ready to proceed yet
+								if !p[0].scf(SCF_ctrlwait) && !p[0].scf(SCF_over) {
+									s.intro = rs4t
+								}
 							}
 						}
 					}
@@ -1387,12 +1399,13 @@ func (s *System) action() {
 							}
 							if !p[0].scf(SCF_over) && !p[0].hitPause() && p[0].alive() && p[0].animNo != 5 {
 								p[0].setSCF(SCF_over)
+								p[0].unsetSCF(SCF_ctrlwait)
 								if p[0].win() {
-									p[0].selfState(180, -1, -1, 1, "")
+									p[0].selfState(180, -1, -1, -1, "")
 								} else if p[0].lose() {
-									p[0].selfState(170, -1, -1, 1, "")
+									p[0].selfState(170, -1, -1, -1, "")
 								} else {
-									p[0].selfState(175, -1, -1, 1, "")
+									p[0].selfState(175, -1, -1, -1, "")
 								}
 							}
 						}

commit 6b7d5a67354014845693bd22a536a056668838af

fix: regression in guarding
- Fixed helpers being able to block even without keyctrl
- Regression in 377e9e9df8055686dbb5172702cbefc783699862

src/char.go
@@ -5618,9 +5618,15 @@ func (c *Char) actionRun() {
 				c.ss.stateType == ST_A && !c.sf(CSF_noairguard)) {
 			c.setSCF(SCF_guard)
 		}
-		if c.inguarddist && c.scf(SCF_guard) && c.cmd[0].Buffer.B > 0 &&
-			!c.inGuardState() {
-			c.changeState(120, -1, -1, "")
+		if c.keyctrl[0] && c.cmd != nil {
+			if c.ctrl() && !c.inputOver() && (c.key >= 0 || c.helperIndex == 0) {
+				if !c.sf(CSF_nohardcodedkeys) {
+					if c.inguarddist && c.scf(SCF_guard) && c.cmd[0].Buffer.B > 0 &&
+						!c.inGuardState() {
+						c.changeState(120, -1, -1, "")
+					}
+				}
+			}
 		}
 	}
 	if sb, ok := c.gi().states[-10]; ok { // still minus 0