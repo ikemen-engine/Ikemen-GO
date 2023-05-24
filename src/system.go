00c330febb787cd3f15b79ad896c9c57f2a2fb82
Lazin3ss <laziness@protonmail.com>
2023-05-24 14:56:14 -0300

    Run lifebar before running the tick frame

--- src/system.go
+++ src/system.go
@@ -1017,159 +1017,8 @@ func (s *System) action() {
 			lowest = (y - s.cam.CameraZoomYBound)
 		}
 	}
-	if s.tickFrame() {
-		s.xmin = s.cam.ScreenPos[0] + s.cam.Offset[0] + s.screenleft
-		s.xmax = s.cam.ScreenPos[0] + s.cam.Offset[0] +
-			float32(s.gameWidth)/s.cam.Scale - s.screenright
-		if s.xmin > s.xmax {
-			s.xmin = (s.xmin + s.xmax) / 2
-			s.xmax = s.xmin
-		}
-		s.allPalFX.step()
-		//s.bgPalFX.step()
-		s.envShake.next()
-		if s.envcol_time > 0 {
-			s.envcol_time--
-		}
-		if s.enableZoomtime > 0 {
-			s.enableZoomtime--
-		} else {
-			s.zoomCameraBound = true
-			s.zoomStageBound = true
-		}
-		if s.super > 0 {
-			s.super--
-		} else if s.pause > 0 {
-			s.pause--
-		}
-		if s.supertime < 0 {
-			s.supertime = ^s.supertime
-			s.super = s.supertime
-		}
-		if s.pausetime < 0 {
-			s.pausetime = ^s.pausetime
-			s.pause = s.pausetime
-		}
-		// in mugen 1.1 most global assertspecial flags are reset during pause
-		// TODO: test if roundnotover should reset (keep intro and noko active)
-		if s.super <= 0 && s.pause <= 0 {
-			s.specialFlag = 0
-		} else {
-			s.unsetSF(GSF_assertspecialpause)
-		}
-		if s.superanim != nil {
-			s.superanim.Action()
-		}
-		s.charList.action(x, &cvmin, &cvmax,
-			&highest, &lowest, &leftest, &rightest)
-		s.nomusic = s.sf(GSF_nomusic) && !sys.postMatchFlg
-	} else {
-		s.charUpdate(&cvmin, &cvmax, &highest, &lowest, &leftest, &rightest)
-	}
-	s.lifebar.step()
-	if s.firstAttack[0] != 0 || s.firstAttack[1] != 0 {
-		s.firstAttack[2] = 1
-	}
-
-	// Action camera
-	leftest -= x
-	rightest -= x
-	var newx, newy float32 = x, y
-	var sclMul float32
-	sclMul = s.cam.action(&newx, &newy, leftest, rightest, lowest, highest,
-		cvmin, cvmax, s.super > 0 || s.pause > 0)
-
-	// Update camera
-	introSkip := false
-	if s.tickNextFrame() {
-		if s.lifebar.ro.cur < 1 && !s.introSkipped {
-			if s.shuttertime > 0 ||
-				s.anyButton() && !s.sf(GSF_roundnotskip) && s.intro > s.lifebar.ro.ctrl_time {
-				s.shuttertime++
-				if s.shuttertime == s.lifebar.ro.shutter_time {
-					s.fadeintime = 0
-					s.resetGblEffect()
-					s.intro = s.lifebar.ro.ctrl_time
-					for i, p := range s.chars {
-						if len(p) > 0 {
-							s.playerClear(i, false)
-							p[0].posReset()
-							p[0].selfState(0, -1, -1, 0, "")
-						}
-					}
-					ox := newx
-					newx = 0
-					leftest = MaxF(float32(Min(s.stage.p[0].startx,
-						s.stage.p[1].startx))*s.stage.localscl,
-						-(float32(s.gameWidth)/2)/s.cam.BaseScale()+s.screenleft) - ox
-					rightest = MinF(float32(Max(s.stage.p[0].startx,
-						s.stage.p[1].startx))*s.stage.localscl,
-						(float32(s.gameWidth)/2)/s.cam.BaseScale()-s.screenright) - ox
-					introSkip = true
-					s.introSkipped = true
-				}
-			}
-		} else {
-			if s.shuttertime > 0 {
-				s.shuttertime--
-			}
-		}
-	}
-	if introSkip {
-		sclMul = 1 / scl
-	}
-	leftest = (leftest - s.screenleft) * s.cam.BaseScale()
-	rightest = (rightest + s.screenright) * s.cam.BaseScale()
-	scl = s.cam.ScaleBound(scl, sclMul)
-	tmp := (float32(s.gameWidth) / 2) / scl
-	if AbsF((leftest+rightest)-(newx-x)*2) >= tmp/2 {
-		tmp = MaxF(0, MinF(tmp, MaxF((newx-x)-leftest, rightest-(newx-x))))
-	}
-	x = s.cam.XBound(scl, MinF(x+leftest+tmp, MaxF(x+rightest-tmp, newx)))
-	if !s.cam.ZoomEnable {
-		// Pos X の誤差が出ないように精度を落とす
-		x = float32(math.Ceil(float64(x)*4-0.5) / 4)
-	}
-	y = s.cam.YBound(scl, newy)
-	s.cam.Update(scl, x, y)
-
-	if s.superanim != nil {
-		s.topSprites.add(&SprData{s.superanim, &s.superpmap, s.superpos,
-			[...]float32{s.superfacing, 1}, [2]int32{-1}, 5, Rotation{}, [2]float32{},
-			false, true, s.cgi[s.superplayer].ver[0] != 1, 1, 1, 0, 0, [4]float32{0, 0, 0, 0}}, 0, 0, 0, 0)
-		if s.superanim.loopend {
-			s.superanim = nil
-		}
-	}
-	for i, pr := range s.projs {
-		for j, p := range pr {
-			if p.id >= 0 {
-				s.projs[i][j].cueDraw(s.cgi[i].ver[0] != 1, i)
-			}
-		}
-	}
-	s.charList.cueDraw()
-	explUpdate := func(edl *[len(s.chars)][]int, drop bool) {
-		for i, el := range *edl {
-			for j := len(el) - 1; j >= 0; j-- {
-				if el[j] >= 0 {
-					s.explods[i][el[j]].update(s.cgi[i].ver[0] != 1, i)
-					if s.explods[i][el[j]].id == IErr {
-						if drop {
-							el = append(el[:j], el[j+1:]...)
-							(*edl)[i] = el
-						} else {
-							el[j] = -1
-						}
-					}
-				}
-			}
-		}
-	}
-	explUpdate(&s.explDrawlist, true)
-	explUpdate(&s.topexplDrawlist, false)
-	explUpdate(&s.underexplDrawlist, true)
-
+	
+	// Run lifebar
 	if s.lifebar.ro.act() {
 		if s.intro > s.lifebar.ro.ctrl_time {
 			s.intro--
@@ -1418,6 +1267,160 @@ func (s *System) action() {
 			s.intro = 0
 		}
 	}
+	
+	// Run tick frame
+	if s.tickFrame() {
+		s.xmin = s.cam.ScreenPos[0] + s.cam.Offset[0] + s.screenleft
+		s.xmax = s.cam.ScreenPos[0] + s.cam.Offset[0] +
+			float32(s.gameWidth)/s.cam.Scale - s.screenright
+		if s.xmin > s.xmax {
+			s.xmin = (s.xmin + s.xmax) / 2
+			s.xmax = s.xmin
+		}
+		s.allPalFX.step()
+		//s.bgPalFX.step()
+		s.envShake.next()
+		if s.envcol_time > 0 {
+			s.envcol_time--
+		}
+		if s.enableZoomtime > 0 {
+			s.enableZoomtime--
+		} else {
+			s.zoomCameraBound = true
+			s.zoomStageBound = true
+		}
+		if s.super > 0 {
+			s.super--
+		} else if s.pause > 0 {
+			s.pause--
+		}
+		if s.supertime < 0 {
+			s.supertime = ^s.supertime
+			s.super = s.supertime
+		}
+		if s.pausetime < 0 {
+			s.pausetime = ^s.pausetime
+			s.pause = s.pausetime
+		}
+		// in mugen 1.1 most global assertspecial flags are reset during pause
+		// TODO: test if roundnotover should reset (keep intro and noko active)
+		if s.super <= 0 && s.pause <= 0 {
+			s.specialFlag = 0
+		} else {
+			s.unsetSF(GSF_assertspecialpause)
+		}
+		if s.superanim != nil {
+			s.superanim.Action()
+		}
+		s.charList.action(x, &cvmin, &cvmax,
+			&highest, &lowest, &leftest, &rightest)
+		s.nomusic = s.sf(GSF_nomusic) && !sys.postMatchFlg
+	} else {
+		s.charUpdate(&cvmin, &cvmax, &highest, &lowest, &leftest, &rightest)
+	}
+	s.lifebar.step()
+	if s.firstAttack[0] != 0 || s.firstAttack[1] != 0 {
+		s.firstAttack[2] = 1
+	}
+
+	// Action camera
+	leftest -= x
+	rightest -= x
+	var newx, newy float32 = x, y
+	var sclMul float32
+	sclMul = s.cam.action(&newx, &newy, leftest, rightest, lowest, highest,
+		cvmin, cvmax, s.super > 0 || s.pause > 0)
+
+	// Update camera
+	introSkip := false
+	if s.tickNextFrame() {
+		if s.lifebar.ro.cur < 1 && !s.introSkipped {
+			if s.shuttertime > 0 ||
+				s.anyButton() && !s.sf(GSF_roundnotskip) && s.intro > s.lifebar.ro.ctrl_time {
+				s.shuttertime++
+				if s.shuttertime == s.lifebar.ro.shutter_time {
+					s.fadeintime = 0
+					s.resetGblEffect()
+					s.intro = s.lifebar.ro.ctrl_time
+					for i, p := range s.chars {
+						if len(p) > 0 {
+							s.playerClear(i, false)
+							p[0].posReset()
+							p[0].selfState(0, -1, -1, 0, "")
+						}
+					}
+					ox := newx
+					newx = 0
+					leftest = MaxF(float32(Min(s.stage.p[0].startx,
+						s.stage.p[1].startx))*s.stage.localscl,
+						-(float32(s.gameWidth)/2)/s.cam.BaseScale()+s.screenleft) - ox
+					rightest = MinF(float32(Max(s.stage.p[0].startx,
+						s.stage.p[1].startx))*s.stage.localscl,
+						(float32(s.gameWidth)/2)/s.cam.BaseScale()-s.screenright) - ox
+					introSkip = true
+					s.introSkipped = true
+				}
+			}
+		} else {
+			if s.shuttertime > 0 {
+				s.shuttertime--
+			}
+		}
+	}
+	if introSkip {
+		sclMul = 1 / scl
+	}
+	leftest = (leftest - s.screenleft) * s.cam.BaseScale()
+	rightest = (rightest + s.screenright) * s.cam.BaseScale()
+	scl = s.cam.ScaleBound(scl, sclMul)
+	tmp := (float32(s.gameWidth) / 2) / scl
+	if AbsF((leftest+rightest)-(newx-x)*2) >= tmp/2 {
+		tmp = MaxF(0, MinF(tmp, MaxF((newx-x)-leftest, rightest-(newx-x))))
+	}
+	x = s.cam.XBound(scl, MinF(x+leftest+tmp, MaxF(x+rightest-tmp, newx)))
+	if !s.cam.ZoomEnable {
+		// Pos X の誤差が出ないように精度を落とす
+		x = float32(math.Ceil(float64(x)*4-0.5) / 4)
+	}
+	y = s.cam.YBound(scl, newy)
+	s.cam.Update(scl, x, y)
+
+	if s.superanim != nil {
+		s.topSprites.add(&SprData{s.superanim, &s.superpmap, s.superpos,
+			[...]float32{s.superfacing, 1}, [2]int32{-1}, 5, Rotation{}, [2]float32{},
+			false, true, s.cgi[s.superplayer].ver[0] != 1, 1, 1, 0, 0, [4]float32{0, 0, 0, 0}}, 0, 0, 0, 0)
+		if s.superanim.loopend {
+			s.superanim = nil
+		}
+	}
+	for i, pr := range s.projs {
+		for j, p := range pr {
+			if p.id >= 0 {
+				s.projs[i][j].cueDraw(s.cgi[i].ver[0] != 1, i)
+			}
+		}
+	}
+	s.charList.cueDraw()
+	explUpdate := func(edl *[len(s.chars)][]int, drop bool) {
+		for i, el := range *edl {
+			for j := len(el) - 1; j >= 0; j-- {
+				if el[j] >= 0 {
+					s.explods[i][el[j]].update(s.cgi[i].ver[0] != 1, i)
+					if s.explods[i][el[j]].id == IErr {
+						if drop {
+							el = append(el[:j], el[j+1:]...)
+							(*edl)[i] = el
+						} else {
+							el[j] = -1
+						}
+					}
+				}
+			}
+		}
+	}
+	explUpdate(&s.explDrawlist, true)
+	explUpdate(&s.topexplDrawlist, false)
+	explUpdate(&s.underexplDrawlist, true)
 
 	if s.tickNextFrame() {
 		spd := s.gameSpeed * s.accel
