e0297b6c295241c08e666ae99f1b5d73c5ad6676
Eiton <micheal257815@gmail.com>
2024-04-17 21:25:29 +0800

    fix: ytension zoom regression Fix the regression where ytension zoom is incorrectly calculated

--- src/camera.go
+++ src/camera.go
@@ -327,7 +327,7 @@ func (c *Camera) action(x, y, scale float32, pause bool) (newX, newY, newScale f
 					newLeft, newRight = targetLeft, targetRight
 					newX = (newLeft + newRight) / 2
 				}
-				newScale = MinF(c.halfWidth*2/(newRight-newLeft), maxScale)
+				newScale = MinF(c.halfWidth*2/(newRight-newLeft), c.zoomin)
 				newY = MinF(MaxF(newY, float32(c.boundhigh)*c.localscl*newScale), float32(c.boundlow)*c.localscl*newScale)
 			} else {
 				newScale = MinF(MaxF(newScale, c.zoomout), c.zoomin)
