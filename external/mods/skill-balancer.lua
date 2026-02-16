813e5a7af9c403f0c85ab025cace377dee9ac8d4
Alfred <git+alfred@omj.dk>
2026-02-16 14:53:38 +0100

    get and set parameters

--- external/mods/skill-balancer.lua
+++ external/mods/skill-balancer.lua
@@ -1,14 +1,14 @@
 -- send http request
 
 local function blocking_httpget(url)
-    local handle = io.popen("curl -s " .. url) -- Is blocking and may cause stutter
-    if not handle then
-        print("Failed to execute curl command")
-        return nil
-    end
-    local result = handle:read("*a")
-    handle:close()
-    return result
+  local handle = io.popen("curl -s " .. url) -- Is blocking and may cause stutter
+  if not handle then
+    print("Failed to execute curl command")
+    return nil
+  end
+  local result = handle:read("*a")
+  handle:close()
+  return result
 end
 
 local response = blocking_httpget("http://localhost:5656/hifromlua")
@@ -17,7 +17,36 @@ print(response)
 response = httpget("http://localhost:5656/hifromgo") -- Using custom go patch function
 print(response)
 
-response = httppost("http://localhost:5656/hifromgo","application/json",'{"name":"Alfred"}') -- Using custom go patch function
+response = httppost("http://localhost:5656/hifromgo", "application/json", '{"name":"Alfred"}') -- Using custom go patch function
 print(response)
 
+function setLevels(p1, p2)
+  if player(1) then
+    setAILevel(p1)
+    print("Player 1 AI level set to " .. ailevel())
+  end
+  if player(2) then
+    setAILevel(p2)
+    print("Player 2 AI level set to " .. ailevel())
+  end
+end
+
+function printLife()
+  if player(1) then
+    print("Player 1 Life: " .. life())
+  end
+  if player(2) then
+    print("Player 2 Life: " .. life())
+  end
+end
+
+addHotkey('p', true, false, false, true, false, 'setCom(1,0)')
+addHotkey('o', true, false, false, true, false, 'setCom(2,0)')
+addHotkey('u', true, false, false, true, false, 'setLevels(1,6)')
+addHotkey('l', true, false, false, true, false, 'printLife(1,6)')
+
+
+
+
+-- setGameSpeed(50)
 print("SB Loaded")
