5b291654d74412260c69f54ec61cc08ff235e47d
K4thos <piotrpit.uek@gmail.com>
2026-03-07 14:11:47 +0100

    fix: margins code for the replay select screen
    Fixes https://discord.com/channels/233345562261323776/233363722934943744/1479655614153232528

--- external/script/main.lua
+++ external/script/main.lua
@@ -2674,6 +2674,18 @@ end
 --replay menu
 function main.f_replay()
 	local w = main.f_menuWindow(motif.replay_info.menu)
+	textImgSetWindow(motif.replay_info.menu.item.selected.active.TextSpriteData, w[1], w[2], w[3], w[4])
+	textImgSetWindow(motif.replay_info.menu.item.active.TextSpriteData, w[1], w[2], w[3], w[4])
+	textImgSetWindow(motif.replay_info.menu.item.value.active.TextSpriteData, w[1], w[2], w[3], w[4])
+	textImgSetWindow(motif.replay_info.menu.item.value.TextSpriteData, w[1], w[2], w[3], w[4])
+	textImgSetWindow(motif.replay_info.menu.item.selected.TextSpriteData, w[1], w[2], w[3], w[4])
+	textImgSetWindow(motif.replay_info.menu.item.TextSpriteData, w[1], w[2], w[3], w[4])
+	for _, v in pairs(motif.replay_info.menu.item.bg) do
+		animSetWindow(v.AnimData, w[1], w[2], w[3], w[4])
+	end
+	for _, v in pairs(motif.replay_info.menu.item.active.bg) do
+		animSetWindow(v.AnimData, w[1], w[2], w[3], w[4])
+	end
 	local cursorPosY = 1
 	local moveTxt = 0
 	local item = 1
