f0a3020684b6d350430dc8e6de16f037f5806922
Rakíel <173973340+RakieI@users.noreply.github.com>
2026-08-28 22:31:30 -0300

    fix: remove redundant positiveBoundhigh usage for motif BGs

--- src/stage.go
+++ src/stage.go
@@ -614,7 +614,7 @@ func (bg backGround) draw(pos [2]float32, drawscl, bgscl, stglscl float32,
 	}
 	// Calculate Y scaling based on vertical scroll position and delta
 	ys2 := bg.scaledelta[1] * pos[1] * bg.delta[1] * bgscl / drawscl / stgscl[1]
-	ys := ((100-(pos[1]-positiveBoundhigh)*bg.yscaledelta)*bgscl/bg.yscalestart)*bg.scalestart[1] + ys2
+	ys := ((100-(pos[1])*bg.yscaledelta)*bgscl/bg.yscalestart)*bg.scalestart[1] + ys2
 	xs := bg.scaledelta[0] * pos[0] * bg.delta[0] * bgscl / stgscl[0]
 	x *= bgscl
 
